bfd7288ab44022478b8535e6819aed668e71cb54
staszarzarstas <royultra77@gmail.com>
2024-10-11 03:31:51 +0300

    second task string

---
+++ codewars_7kyu/move_10.js
@@ -0,0 +1,8 @@
+// Move every letter in the provided string forward 10 letters through the alphabet.
+// If it goes past 'z', start again at 'a'.
+// Input will be a string with length > 0.
+
+const alphabetLower = 'abcdefghijklmnopqrstuvwxyz';
+function moveTen(s){
+    s.split('')
+}
